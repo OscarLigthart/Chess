745b215176f6212fdee6f565e427f58c0103fb7a
Oscar Ligthart <oscarligthart@gmail.com>
2021-08-17 18:18:42 +0200

    now correctly loading board

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,4 @@
+#include <iostream>
 #include <GLFW/glfw3.h>
 #include <SFML/Graphics.hpp>
 #include <time.h>
@@ -15,31 +16,13 @@
 //     {  1, 2, 3, 4, 5, 3, 2, 1 }
 // };
 
-// int main()
-// {
-//     sf::RenderWindow game(sf::VideoMode(640, 640), "Chess");
-//     sf::Texture t1,t2;
-//     t1.loadFromFile("img/pieces.png");
-//     t2.loadFromFile("img/board.png");
-//     // sf::Sprite s;
-//     sf::Sprite sBoard(t2);
-//     // s.setTexture(t1);
-
-//     while (game.isOpen()) {
-
-//         game.clear();
-//         game.draw(sBoard);
-//         game.display();
-
-//     }
-// }
 
 int main() {
     // Declare and create a new render-window
-    sf::RenderWindow window(sf::VideoMode(800, 800), "Chess");
+    sf::RenderWindow window(sf::VideoMode(1000, 1000), "Chess");
 
     sf::Texture b;
-    b.loadFromFile("img/board.png");
+    b.loadFromFile("../img/board.png");    
     sf::Sprite Board(b);
 
     // The main loop - ends as soon as the window is closed
