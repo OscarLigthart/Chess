b5ec1450d589f05ceb2092f747cfa6950d67a51e
Oscar Ligthart <oscarligthart@gmail.com>
2022-01-09 18:22:46 +0100

    moved rook to use header files

---
+++ include/pieces/rook.hpp
@@ -0,0 +1,32 @@
+/**
+ *  Class that holds the base logic of a piece
+ */
+
+#ifndef PIECE_ROOK
+#define PIECE_ROOK
+
+#include <SFML/Graphics.hpp>
+
+class Rook: public Piece 
+{           // The class
+  public:               // Access specifier
+    sf::Sprite sprite;  // the sprite for the piece
+    Rook (int player)
+
+    /**
+     *  Method to check if move is valid
+     */ 
+    bool isValidMove(int y, int x)
+
+    /** 
+     *  Method to retrieve the sprite
+     */ 
+    sf::Sprite getSprite() { return this->sprite; }
+
+    /**
+     *  Method to update sprite position
+     */ 
+    void setSpritePosition(int x, int y)
+};
+
+#endif
