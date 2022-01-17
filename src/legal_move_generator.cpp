90944dc3d6152ec611ec53f95f1b53a6ab625bc9
Oscar Ligthart <oscarligthart@gmail.com>
2022-01-17 13:28:24 +0100

    wrote pseudo outline

--- src/legal_move_generator.cpp
+++ src/legal_move_generator.cpp
@@ -11,3 +11,14 @@
 LegalMoveGenerator::LegalMoveGenerator() {
 
 }
+
+// --> IN THIS CLASS    
+// given a player, all the legal moves should be calculated
+
+// --> IN THE PIECE CLASSES
+// to do so, we will need to get all possible moves from the pieces themselves, 
+// these will have to be sorted based on closest to farthest move
+
+// How to split the board and legalmovegenerator?
+
+// What to do with captures?
