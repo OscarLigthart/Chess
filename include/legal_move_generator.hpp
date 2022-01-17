c92204640c96e6b4d034069dff321812195085bc
Oscar Ligthart <oscarligthart@gmail.com>
2022-01-17 12:39:36 +0100

    created legal move generator outline

---
+++ include/legal_move_generator.hpp
@@ -0,0 +1,23 @@
+/**
+ *  Header of the legal move generator
+ *  This class takes the board and decides all the valid moves for one player
+ */
+
+#ifndef LEGAL_MOVE_GENERATOR
+#define LEGAL_MOVE_GENERATOR
+
+#include <board.hpp>
+
+
+class LegalMoveGenerator 
+  {
+    public:
+
+    /**
+     *  Constructor
+     */    
+    LegalMoveGenerator();
+
+};
+
+#endif
---
+++ src/legal_move_generator.cpp
@@ -0,0 +1,13 @@
+/**
+ *  Implementation of the legal move generator
+ *  This class takes the board and decides all the valid moves for one player
+ */
+
+#include <legal_move_generator.hpp>
+
+/**
+ *  Constructor
+ */
+LegalMoveGenerator::LegalMoveGenerator() {
+
+}
