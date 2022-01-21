6f89efb94126af20197ed72fd5fcb15c7f3a2bda
Oscar Ligthart <oscarligthart@gmail.com>
2022-01-21 13:36:05 +0100

    implemented get moves for king

--- src/pieces/king.cpp
+++ src/pieces/king.cpp
@@ -37,6 +37,51 @@ bool King::isValidMove(int y, int x){
     }
 }
 
+/**
+ *  Method that retrieves all moves for this piece
+ *
+ *  @return list of arrays depicting a diagonal in coordinates
+ */ 
+std::vector<std::array<int, 2>> Queen::getMoves(int board[8][8]) {
+
+    // generate the directions to move in
+    int directions[8][2] = {{-1,0},{1,0},{0,-1}, {0,1},{-1,1},{1,1},{-1,-1},{1, -1}};
+
+    // initialize valid move list
+    std::vector<std::array<int,2>> moves;
+
+    // set the current position to the actual position
+    std::array<int,2> start = {this->y, this->x};
+
+    // loop over diagonals
+    for (int i=0; i<8; i++) {
+        
+        // take current position and add the direction
+        std::array<int,2> new_move = {start[0] + directions[i][0], start[1] + directions[i][1]};
+
+        // start by checking for out of bounds
+        bool outOfBounds = this->checkOutOfBounds(new_move[1], new_move[0]);
+
+        // stop if we are out of bounds
+        if (outOfBounds) { break; }
+        
+        // retrieve the piece that is placed on the square of the new move
+        int square = board[new_move[0]][new_move[1]];
+
+        // check for collisions
+        bool collision = this->checkCollision(moves, new_move, square);
+
+        // if we collided we should stop
+        if (collision) { break; }
+
+        // if we reach this we can add the move to the total moves
+        moves.push_back(new_move);
+    }
+
+    // the total set of valid moves
+    return moves;
+}
+
 void King::setSpritePosition(int x, int y) { 
     
     // change position of sprite
